53ee86952723552b3c5bf759f8032dbd1d16b2b1
wavilap <wavilap0509@gmail.com>
2018-01-22 14:37:28 -0500

    Add Props to components

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Media from './src/playlist/components/media'
+
+const app = document.getElementById('app');
+{/*const holaMundo = <h1>Hello Worldss</h1>;*/}
+
+// ReactDOM.render(que voy a renderizar, donde lo haré);
+ReactDOM.render(<Media title="¿Qué es?" author="Leonidas Esteban" image="./images/covers/bitcoin.jpg"/>, app);
+
+// console.log('Hello World');
---
+++ src/playlist/components/media.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './media.css';
+
+class Media extends Component {
+	render() {
+		const styles = {
+			container: {
+				color: '#44546b',
+				cursor: 'pointer',
+				width: 260,
+				border: '1px solid red'
+			}
+		}
+		return (
+			<div className="Media">
+				<div className="Media-cover">
+					<img
+						src={this.props.image}
+						alt=""
+						width={260}
+						height={160}
+						className="Media-image"
+					/>
+					<h3 className="Media-title">{this.props.title}</h3>
+					<p className="Media-author">{this.props.author}</p>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Media;
